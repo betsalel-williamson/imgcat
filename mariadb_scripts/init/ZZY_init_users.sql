4f01960d65ccdc5469132aebe86627c4254ee8ef
Azulflame <toddbjta@gmail.com>
2025-09-05 14:32:31 -0500

    Renamed files for alphabetical odering during mariadb init sequence. Files are run alphabetically and some need a certain order.

--- mariadb_scripts/init/ZZY_init_users.sql
+++ mariadb_scripts/init/ZZY_init_users.sql
@@ -1,21 +1,19 @@
 CREATE USER imgcat_jwt
-IDENTIFIED BY <password here>
+IDENTIFIED BY 'imgcat_jwt'
 WITH
 	MAX_STATEMENT_TIME 1;
 
 CREATE USER imgcat_posts
-IDENTIFIED BY <password here>
+IDENTIFIED BY 'imgcat_posts'
 WITH
 	MAX_STATEMENT_TIME 5;
 
 CREATE USER imgcat_mods
-IDENTIFIED BY <password here>
+IDENTIFIED BY 'imgcat_mods'
 WITH
 	MAX_STATEMENT_TIME 5;
 
 CREATE USER imgcat_system
-IDENTIFIED BY <password here>
+IDENTIFIED BY 'imgcat_system'
 WITH
 	MAX_STATEMENT_TIME 5;
-
-# Moved GRANT block to ./init/init_perms.sql
