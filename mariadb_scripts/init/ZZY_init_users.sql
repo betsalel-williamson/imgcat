0773d73666472cdb2839901af2b17be4192566af
Azulflame <toddbjta@gmail.com>
2025-09-05 14:44:32 -0500

    Return user init to a state with invalid passwords

--- mariadb_scripts/init/ZZY_init_users.sql
+++ mariadb_scripts/init/ZZY_init_users.sql
@@ -1,19 +1,19 @@
 CREATE USER imgcat_jwt
-IDENTIFIED BY 'imgcat_jwt'
+IDENTIFIED BY <password here>
 WITH
 	MAX_STATEMENT_TIME 1;
 
 CREATE USER imgcat_posts
-IDENTIFIED BY 'imgcat_posts'
+IDENTIFIED BY <password here>
 WITH
 	MAX_STATEMENT_TIME 5;
 
 CREATE USER imgcat_mods
-IDENTIFIED BY 'imgcat_mods'
+IDENTIFIED BY <password here>
 WITH
 	MAX_STATEMENT_TIME 5;
 
 CREATE USER imgcat_system
-IDENTIFIED BY 'imgcat_system'
+IDENTIFIED BY <password here>
 WITH
 	MAX_STATEMENT_TIME 5;
